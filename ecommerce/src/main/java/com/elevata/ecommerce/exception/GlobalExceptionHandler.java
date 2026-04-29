a47fabba48d587723e6b9d95274a9e86fc8c3349
Afroz Mansuri <lergapp@gmail.com>
2026-04-29 19:09:54 +0530

    added product handling exception

--- ecommerce/src/main/java/com/elevata/ecommerce/exception/GlobalExceptionHandler.java
+++ ecommerce/src/main/java/com/elevata/ecommerce/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.elevata.ecommerce.exception;
 import com.elevata.ecommerce.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,6 +14,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException exception) {
         ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), exception.getMessage(), "User not found");
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleProductNotFound(ProductNotFoundException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), exception.getMessage(), "Product not found");
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 }
