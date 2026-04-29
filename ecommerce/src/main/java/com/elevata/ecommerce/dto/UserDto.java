a68f6b6122e3b6cd5790cf12862077b16c06efb3
Afroz Mansuri <lergapp@gmail.com>
2026-04-29 18:35:10 +0530

    added validation for name, phone no. and pincode

--- ecommerce/src/main/java/com/elevata/ecommerce/dto/UserDto.java
+++ ecommerce/src/main/java/com/elevata/ecommerce/dto/UserDto.java
@@ -1,5 +1,7 @@
 package com.elevata.ecommerce.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,9 +9,18 @@ import lombok.Setter;
 @Getter @Setter
 @AllArgsConstructor
 public class UserDto {
-        int userId;
-        String name;
-        String phoneNumber;
-        int pincode;
-        String address;
+    private int userId;
+
+    @Pattern(regexp = "^[A-Za-z]\\w{5,29}$")
+    private String name;
+
+    @Pattern(regexp = "^[6-9][0-9]{9}$", message = "Invalid mobile number")
+    @Size(min = 10, max = 10, message = "Mobile number must be 10 digits")
+    private String phoneNumber;
+
+    @Pattern(regexp = "^[1-9][0-9]{5}$", message = "Invalid pincode")
+    @Size(min = 6, max = 6, message = "Pincode must be 6 digits")
+    private int pincode;
+
+    private String address;
 }
