4b3d442f1ae8eec4456278b6ca3e25886517a44f
Afroz Mansuri <lergapp@gmail.com>
2026-05-03 12:27:25 +0530

    added a handler for validation errors

--- ecommerce/src/main/java/com/elevata/ecommerce/exception/GlobalExceptionHandler.java
+++ ecommerce/src/main/java/com/elevata/ecommerce/exception/GlobalExceptionHandler.java
@@ -3,13 +3,26 @@ package com.elevata.ecommerce.exception;
 import com.elevata.ecommerce.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationErrors(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException exception) {
