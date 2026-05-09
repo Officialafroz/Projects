489110919a0085fb8ccdd91056bb47e99c17097d
Afroz Mansuri <lergapp@gmail.com>
2026-05-09 16:42:21 +0530

    added validation annotations

--- ecommerce/src/main/java/com/elevata/ecommerce/dto/UserRegistrationDto.java
+++ ecommerce/src/main/java/com/elevata/ecommerce/dto/UserRegistrationDto.java
@@ -1,13 +1,17 @@
 package com.elevata.ecommerce.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class UserRegistrationDto {
     private String name;
-    //Regex
+
+    @Pattern(regexp = "^[6-9][0-9]{9}$", message = "Invalid mobile number")
     private String phoneNumber;
-    //Regex
-    private int pincode;
+
+    @Pattern(regexp = "^[1-9][0-9]{5}$", message = "Invalid postal code. Must begin with 1 to 9 and have 6 digits.")
+    private String pincode;
+
     private String address;
 }
