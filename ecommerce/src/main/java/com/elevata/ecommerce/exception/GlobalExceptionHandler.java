2f9aa1e97223c31192d88f415ab6062c0f054f03
Afroz Mansuri <lergapp@gmail.com>
2026-05-11 09:45:54 +0530

    added a handler for database errors, replaced 3 args constructor with 2 args

--- ecommerce/src/main/java/com/elevata/ecommerce/exception/GlobalExceptionHandler.java
+++ ecommerce/src/main/java/com/elevata/ecommerce/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.elevata.ecommerce.exception;
 
 import com.elevata.ecommerce.dto.ErrorResponse;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -24,27 +26,36 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(errors);
     }
 
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<ErrorResponse> handleDataAccessException(DataAccessException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(LocalDateTime.now() ,
+                        "Database error",
+                        List.of(exception.getMostSpecificCause().getMessage())));
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException exception) {
-        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), exception.getMessage(), "Resource not found");
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), exception.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException exception) {
-        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), exception.getMessage(), "User not found");
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), exception.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleProductNotFound(ProductNotFoundException exception) {
-        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), exception.getMessage(), "Product not found");
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), exception.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(StoreNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleStoreNotFound(StoreNotFoundException exception) {
-        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), exception.getMessage(), "Store not found.");
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), exception.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 }
