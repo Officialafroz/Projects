989ce3e4609fda3c2256fcb88b871fc8c43de2f5
Afroz Mansuri <lergapp@gmail.com>
2026-05-11 09:44:18 +0530

    updated details datatype, added a 2 args constructor for handling responses

--- ecommerce/src/main/java/com/elevata/ecommerce/dto/ErrorResponse.java
+++ ecommerce/src/main/java/com/elevata/ecommerce/dto/ErrorResponse.java
@@ -3,16 +3,21 @@ package com.elevata.ecommerce.dto;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 public class ErrorResponse {
     private LocalDateTime timestamp;
     private String message;
-    private String details;
+    private List<String> details;
 
-    public ErrorResponse(LocalDateTime timestamp, String message, String details) {
+    public ErrorResponse(LocalDateTime timestamp, String message, List<String> details) {
+        this(timestamp, message);
+        this.details = details;
+    }
+
+    public ErrorResponse(LocalDateTime timestamp, String message) {
         this.timestamp = timestamp;
         this.message = message;
-        this.details = details;
     }
 }
